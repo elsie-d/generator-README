598f41be1e0be42cb599a4cebe15b786323e0ba6
unknown <elsie.dh@gmail.com>
2023-11-21 02:13:45 -0800

    working generator

--- index.js
+++ index.js
@@ -1,1 +1,148 @@
 console.log('test')
+
+// TODO: Include packages needed for this application
+const inquire = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
+// TODO: Create an array of questions for user input
+const questions =
+        [{
+            type: 'input',
+            message: 'What is the tilte of your project?',
+            name: 'title',
+            validate: (title) => {
+                if (title) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Please share a brief description of your project',
+            name: 'description',
+            validate: (description) => {
+                if (description) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Any installation requirements? (Type N/A if none required)',
+            name: 'installation',
+            validate: (installation) => {
+                if (installation) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What do we need to know for usage?',
+            name: 'usage',
+            validate: (usage) => {
+                if (usage) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'How can others contribute?',
+            name: 'contributions',
+            validate: (contributions) => {
+                if (contributions) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'How can we test the project?',
+            name: 'tests',
+            validate: (test) => {
+                if (test) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'list',
+            message: 'Chose a license for your application:',
+            choices: ['MIT', 'ISC', 'GNU General Public License v3.0', 'Apache 2.0 License', 'None'],
+            name: 'license'
+            
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'gitUser',
+            validate: (gitUser) => {
+                if (gitUser) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is you email address?',
+            name: 'email',
+            validate: (email) => {
+                if (email) {
+                    return true
+                }
+                else {
+                    return 'Value entry required'
+                }
+            }
+        },
+        
+        
+    ]
+        
+   
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err)=>
+    err ? console.log(err): console.log(`README file is ready`)
+    )
+  }
+
+// const writeFileAsync = util.promisify(writeToFile);
+
+// TODO: Create a function to initialize app
+function init() {
+    inquire.prompt(questions).then((answers) => {
+      console.log("Working...");
+      writeToFile("README.md", generateMarkdown({ ...answers }));
+    });
+  }
+
+// Function call to initialize app
+init(); 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,16 +5,16 @@ function renderLicenseBadge(license) {
   
     switch (license) {
     case 'MIT':
-        badge='-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n'
+        badge='[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n'
     break;
     case 'ISC':
-        badge='-[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)\n\n'
+        badge='[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)\n\n'
     break;
     case 'GNU General Public License v3.0':
-        badge='-[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n';
+        badge='[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n';
     break;
     case 'Apache 2.0 License':
-        badge='-[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n';
+        badge='[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n';
     break;      
     default:
         badge="";
@@ -32,7 +32,7 @@ function renderLicenseBadge(license) {
   }
   else {
       return `## License
-  ${license}` 
+  -${license}` 
   }
   
   }
@@ -52,43 +52,44 @@ function renderLicenseBadge(license) {
   
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
-    return `# ${data.title}
-    ${renderLicenseBadge(data.license)}
+    return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
     
-    ## Description
-    ${data.description}
+## Description
+${data.description}
             
-    ## Table of Contents
+## Table of Contents
             
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributions)
-    - [Tests](#tests)
-    ${renderLicenseLink(data.license)}
-    - [Questions](#questions)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributions)
+- [Tests](#tests)
+${renderLicenseLink(data.license)}
+- [Questions](#questions)
             
-    ## Installation
-    ${data.installation}
-            
-            
-    ## Usage
-    ${data.usage}
-            
-    ## Contributions
-    ${data.contributions}
-            
-    ## Tests
-    ${data.tests}
-            
-    ${renderLicenseSection(data.license)}
+## Installation
+${data.installation}
             
             
-    ## Questions?
-    Find me on GitHub: <a href="https://github.com/${data.gitUser}" target="_blank">${data.gitUser}</a>
+## Usage
+${data.usage}
             
-    OR
+## Contributions
+${data.contributions}
             
-    Email me at: <a href="mailto:${data.email}" target="_blank">${data.email}</a>
+## Tests
+${data.tests}
+            
+${renderLicenseSection(data.license)}
+            
+            
+## Questions?
+Find me on GitHub: <a href="https://github.com/${data.gitUser}" target="_blank">${data.gitUser}</a>
+            
+OR
+            
+Email me at: <a href="mailto:${data.email}" target="_blank">${data.email}</a>
   `;
   }
   
